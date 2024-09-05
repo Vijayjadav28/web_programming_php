54001d5061ed370bd3c0a1a18ed73b0a604d1197
Vijayjadav28 <vijayjadav2863@gmail.com>
2024-09-05 21:08:16 +0530

    Add files via upload

---
+++ task6.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html and php</title>
+</head>
+<body>
+    <?php 
+    echo "<h1>This is Heading</h1>";
+    echo "<p><b>This Text is Bold</b></p>";
+    echo "<p><i>This Text is Italic</i></p>";
+    ?>
+</body>
+</html>
